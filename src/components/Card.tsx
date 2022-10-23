432ac31094341a871a386807dc8049a3df3d7c0d
Yahyaoui Nabil <yahyaouinabil1993@gmail.com>
2022-10-23 23:37:50 +0200

    build: create products component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable prettier/prettier */
 import React from "react"
-import products from "../../build/public/products.json"
 import CTA from "./CTA"
 import Products from "./Products"
 
@@ -8,9 +7,7 @@ const Card = () => {
     return (
         <div className="absolute top-[176px] left-[440px] w-560 h-511">
             <div className="container absolute left-0 top-0 w-560 h-511 bg-card-bg rounded-lg shadow-5xl">
-                {products.map((product) => (
-                    <Products key={product.id} name={product.name} />
-                ))}
+                <Products />
             </div>
             <CTA />
         </div>
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,9 +1,18 @@
 import React from "react"
+import products from "../../build/public/products.json"
 
-const Products = ({ name }) => {
+const Products = () => {
     return (
-        <div className="flex">
-            <h5>{name}</h5>
+        <div className="absolute top-[18px] left-[149px] w-262 h-86 flex">
+            <div className="absolute left-0 top-0 w-86 h-86 cursor-pointer">
+                <img src={products[1].image} alt={products[0].name} />
+            </div>
+            <div className="absolute left-[86px] top-[9px] w-[74px] h-[69px] cursor-pointer">
+                <img src={products[2].image} alt={products[0].name} />
+            </div>
+            <div className="absolute left-[175px] top-[9px] w-[69px] h-[69px] cursor-pointer">
+                <img src={products[0].image} alt={products[0].name} />
+            </div>
         </div>
     )
 }
